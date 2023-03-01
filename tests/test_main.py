c2066de3b49a83967c35f7a7a69d582d0a213d7e
nvLast86 <n.lastenko@gmail.com>
2023-03-01 12:55:04 +0600

    changed test_main.py

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,31 +1,16 @@
 import unittest
-from utils.node import Node
 from utils.stack import Stack
 
 
-class test_Node(unittest.TestCase):
-    def setUp(self):
-        self.A = (5, None)
-        self.B = ('a', self.A)
+class test_data(unittest.TestCase):
+    stack = Stack()
+    stack.push('data1')
+    stack.push('data2')
+    stack.push('data3')
 
-    def test_init(self):
-        self.assertEqual((self.A[0], self.A[1]), (5, None))
-        self.assertEqual((self.B[0], self.B[1]), ('a', self.A))
-
-
-class test_Stack(unittest.TestCase):
-
-    def setUp(self):
-        self.A = (5, None)
-        self.test_data = 'data1'
-
-    def test_init(self):
-        self.assertEqual(self.A, (5, None))
-
-    def test_push(self):
-        self.assertEqual(self.test_data, 'data1')
+    def test_data(self):
+        self.assertEqual(self.stack.top.data, 'data3')
+        self.assertEqual(self.stack.top.next_node.data, 'data2')
+        self.assertEqual(self.stack.top.next_node.next_node.data, 'data1')
 
 
-
-if __name__ == '__main__':
-    unittest.main()

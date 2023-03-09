db2c145ce3d6053ec8b8468d9062cf2ddf6258a0
nvLast86 <n.lastenko@gmail.com>
2023-03-09 11:54:39 +0600

    stack.py has been corrected.

--- utils/stack.py
+++ utils/stack.py
@@ -2,12 +2,24 @@ from utils.node import Node
 
 
 class Stack:
+    """Класс, реализующий структуру данных, представляющая из себя упорядоченный набор элементов,
+    в которой добавление новых элементов и удаление существующих производится с одного конца,
+    называемого вершиной стека. Притом первым из стека удаляется элемент, который был помещен туда последним,
+    то есть в стеке реализуется стратегия «последним вошел — первым вышел» (last-in, first-out — LIFO).
+    """
 
     def __init__(self):
-        self.top = Node()
+        self.elements = []
+        self.top = None
 
     def push(self, data):
-        new_node = Node(data)
-        new_node.next_node = self.top
+        """
+        Добавляет в стэк элемент в виде узла Node
+        """
+        if len(self.elements) == 0:
+            new_node = Node(data)
+        else:
+            new_node = Node(data, self.top)
         self.top = new_node
+        return self.elements.append(new_node)
 

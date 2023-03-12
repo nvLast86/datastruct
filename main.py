ac1a2dc635ed33935f2259a9fd53b2f01a4f9542
nvLast86 <n.lastenko@gmail.com>
2023-03-13 00:23:53 +0600

    main.py has been corrected according to task #2

--- main.py
+++ main.py
@@ -1,21 +1,26 @@
 from utils.node import Node
 from utils.stack import Stack
 
+stack = Stack()
+stack.push('data1')
+data = stack.pop()
 
-n1 = Node(5, None)
-n2 = Node('a', n1)
-print(n1.data)
-print(n2.data)
-print(n1)
-print(n2.next_node)
+# стэк стал пустой
+print(stack.top)
+
+# pop() удаляет элемент и возвращает данные удаленного элемента
+print(data)
+
 
 stack = Stack()
 stack.push('data1')
 stack.push('data2')
-stack.push('data3')
+data = stack.pop()
+
+# теперь последний элемента содержит данные data1
 print(stack.top.data)
-print(stack.top.next_node.data)
-print(stack.top.next_node.next_node.data)
-print(stack.top.next_node.next_node.next_node)
-print(stack.top.next_node.next_node.next_node.data)
+
+# данные удаленного элемента
+print(data)
+
 

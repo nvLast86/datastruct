f8fbd1ab9b0b692f5144c9f149787c6e3b6c7e7a
nvLast86 <n.lastenko@gmail.com>
2023-03-16 13:24:51 +0600

    method dequeue() has been added according to task #4

--- utils/queue.py
+++ utils/queue.py
@@ -20,3 +20,16 @@ class Queue:
         else:
             self.tail.next_node = new_node
             self.tail = new_node
+
+    def dequeue(self):
+        """
+        Удаляет из очереди крайний левый элемент (первый добавленный)
+        :return: Удаленный экземпляр класса Node
+        """
+        if self.head is None:
+            return None
+        else:
+            deleted_node_object = self.head.data
+            self.head = self.head.next_node
+            return deleted_node_object
+

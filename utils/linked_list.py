73136b7b376f3b34f88a0a1c6866f3f4340e03fc
nvLast86 <n.lastenko@gmail.com>
2023-03-20 09:43:41 +0600

    code of methods has been added

--- utils/linked_list.py
+++ utils/linked_list.py
@@ -6,14 +6,43 @@ class LinkedList:
     т.е. на первый и последний Node
     """
 
-    def __init__(self, data):
+    def __init__(self):
         self.head = None
         self.tail = None
 
-    def insert_beginning(self):
+    def insert_beginning(self, data):
+        new_node = Node(data)
+        if self.head is None and self.tail is None:
+            self.tail = new_node
+        new_node.next_node = self.head
+        self.head = new_node
 
+    def insert_at_end(self, data):
+        new_node = Node()
+        if self.head is None and self.tail is None:
+            self.head = new_node
+        else:
+            self.tail.next_node = new_node
+        self.tail = new_node
 
-    def insert_at_end(self):
+    def print_ll(self):
+        ll_string = ''
+        node = self.head
+        if node is None:
+            print(None)
+        while node:
+            ll_string += f' {str(node.data)} ->'
+            node = node.next_node
+
+        ll_string += ' None'
+        print(ll_string)
+
+ll = LinkedList()
+ll.insert_beginning({'id': 1})
+ll.insert_at_end({'id': 2})
+ll.insert_at_end({'id': 3})
+ll.insert_beginning({'id': 0})
+ll.print_ll()
 
 
 

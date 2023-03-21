facc00214b1f6f54525812d2422c1f7870e122f1
nvLast86 <n.lastenko@gmail.com>
2023-03-22 05:39:24 +0600

    little fixes

--- utils/linked_list.py
+++ utils/linked_list.py
@@ -51,12 +51,12 @@ class LinkedList:
         print(ll_string)
 
 
-ll = LinkedList()
-ll.insert_beginning({'id': 1})
-ll.insert_at_end({'id': 2})
-ll.insert_at_end({'id': 3})
-ll.insert_beginning({'id': 0})
-ll.print_ll()
+# ll = LinkedList()
+# ll.insert_beginning({'id': 1})
+# ll.insert_at_end({'id': 2})
+# ll.insert_at_end({'id': 3})
+# ll.insert_beginning({'id': 0})
+# ll.print_ll()
 
 
 

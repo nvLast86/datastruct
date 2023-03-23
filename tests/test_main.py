d972c20895647e7530dc2af88435d1dc41634412
nvLast86 <n.lastenko@gmail.com>
2023-03-23 15:57:14 +0600

    tests of get_data_by_id() have been added

--- tests/test_main.py
+++ tests/test_main.py
@@ -5,6 +5,7 @@ from utils.linked_list import LinkedList
 from io import StringIO
 from contextlib import redirect_stdout
 
+
 class test_data(unittest.TestCase):
     stack = Stack()
     stack.push('data1')
@@ -46,7 +47,7 @@ class TestQueue(unittest.TestCase):
         assert queue.dequeue() is None
 
 
-class Linked_listtest(unittest.TestCase):
+class TestLinked_list(unittest.TestCase):
 
     def test_print_ll(self):
         ll = LinkedList()
@@ -54,12 +55,35 @@ class Linked_listtest(unittest.TestCase):
         ll.insert_at_end({'id': 2})
         ll.insert_at_end({'id': 3})
         ll.insert_beginning({'id': 0})
-        self.assertEqual(ll.print_ll(), None)
-
-
-
 
         out = StringIO()
         with redirect_stdout(out):
             ll.print_ll()
         self.assertEqual(out.getvalue().strip(), "{'id': 0} -> {'id': 1} -> {'id': 2} -> {'id': 3} -> None")
+
+
+    def test_to_list(self):
+        ll1 = LinkedList()
+        ll1.insert_beginning({'id': 1, 'username': 'lazzy508509'})
+        ll1.insert_at_end({'id': 2, 'username': 'mik.roz'})
+        ll1.insert_at_end({'id': 3, 'username': 'mosh_s'})
+        ll1.insert_beginning({'id': 0, 'username': 'serebro'})
+        test_list = ll1.to_list()
+        self.assertEqual(len(test_list), 4)
+        self.assertEqual(test_list[0]['id'], 0)
+        self.assertEqual(test_list[1]['id'], 1)
+        self.assertEqual(test_list[2]['id'], 2)
+        self.assertEqual(test_list[3]['id'], 3)
+
+    def test_get_data_by_id(self):
+        ll2 = LinkedList()
+        ll2.insert_beginning({'id': 1, 'username': 'lazzy508509'})
+        ll2.insert_at_end({'id': 2, 'username': 'mik.roz'})
+        ll2.insert_at_end({'id': 3, 'username': 'mosh_s'})
+        ll2.insert_beginning({'id': 0, 'username': 'serebro'})
+        self.assertEqual(ll2.get_data_by_id(0), {'id': 0, 'username': 'serebro'})
+        self.assertEqual(ll2.get_data_by_id(1), {'id': 1, 'username': 'lazzy508509'})
+        self.assertEqual(ll2.get_data_by_id(2), {'id': 2, 'username': 'mik.roz'})
+        self.assertEqual(ll2.get_data_by_id(3), {'id': 3, 'username': 'mosh_s'})
+        self.assertRaises(TypeError, ll2.get_data_by_id, 'n')
+
